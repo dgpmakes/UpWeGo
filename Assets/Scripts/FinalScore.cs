0489cdceb25db9ff61622245608b9dcaa8e8e901
dgpMakes <dgpmakes@gmail.com>
2020-10-10 15:14:20 +0200

    Initial commit

---
+++ Assets/Scripts/FinalScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // include UI namespace so can reference UI elements
+
+public class FinalScore : MonoBehaviour
+{
+    public Text UIScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Up.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Up : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 1 * Time.deltaTime, 0);
+    }
+}
